7fc897286d57a050a189440f5d6e909ec8e6d72e
Cavidismailzadeh <119400508+Cavidismailzadeh@users.noreply.github.com>
2022-12-07 22:00:01 +0400

    CSharpTaskHomeWork

---
+++ CSharpTask/CSharpTask/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+//FirstTask
+
+//int n = 21;
+//if (n % 21 == 0) ;
+//{
+//    Console.WriteLine("bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+
+
+//SecondTask
+
+//int n = 2;
+//int m = 8;
+
+//if
+//    (n % 2 == 0 && m % 2 == 0)
+//{
+//    Console.WriteLine(n + m);
+//}
+//else
+//{
+//    Console.WriteLine("Cut reqem deyil")
+//}
+
+
+
+
+
+
+
+////ThirdTask
+
+
+//int n = 5;
+//int m = 13;
+//int sum = 0; 
+
+//for (int i = 0; i < m; i++)
+//{
+//	if (i % 2 == 1)
+//	{
+//		sum += i;
+//Console.WriteLine(sum)
+//	}
+
+
+
+
+//FourthTask
+
+
+//int[] nums = { 1, 43, 24, 13, 16, 19, 21 };
+//int sum = 0;
+
+//foreach (var num in nums);
+//{
+//	if (num%2==1) sum+=num;
+//	{
+
+//    Console.WriteLine(num);  
+//	}
+
+//}
+//else
+//{
+//	Console.WriteLine("Tek reqem yoxdur");
+//}
+
+
+
+//FifthTask
+
+
+//int[] nums = { 1, 43, 24, 13, 16, 19, 21, 32,75,64,97,102};
+//int count = 0;
+
+//foreach (var num in nums)
+//{
+//	if (num % 2 == 0) ;
+//	count++;
+//	{
+//		Console.WriteLine(count);
+//	}
